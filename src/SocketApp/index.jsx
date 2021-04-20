30f4532df272386ce23ad6f1b7d1190409fd6f06
AJTJ <janke.aaron@gmail.com>
2021-04-20 11:08:20 -0400

    update

--- src/SocketApp/index.jsx
+++ src/SocketApp/index.jsx
@@ -2,34 +2,62 @@ import React, { useState, useRef, useEffect } from "react";
 
 const AppWs = () => {
   const [isPaused, setPause] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [receivedData, setReceivedData] = useState(undefined);
   const [inputValue, setInputValue] = useState(undefined);
   const ws = useRef(null);
 
-  useEffect(() => {
-    ws.current = new WebSocket("ws://127.0.0.1:8081/ws/");
-    console.log(ws.current);
+  const socket_address = "ws://127.0.0.1:8081/ws/";
+
+  const openSocket = () => {
+    ws.current = new WebSocket(socket_address);
+    console.log(`current: ${ws.current}`);
     ws.current.onopen = () => {
       console.log("ws opened");
-      // ws.current.send("dog");
     };
     ws.current.onclose = () => console.log("ws closed");
+  };
+
+  // open the socket on page load
+  useEffect(() => {
+    openSocket();
+    setIsOpen(true);
 
     return () => {
-      ws.current.close();
+      // close the socket if it exists
+      ws?.current?.close();
     };
   }, []);
 
+  // handle messaging
   useEffect(() => {
-    if (!ws.current) return;
+    if (!ws.current) {
+      console.log("no ws.current");
+    }
 
     ws.current.onmessage = (e) => {
-      if (isPaused) return;
+      console.log("inside onmessage");
+      if (isPaused) {
+        console.log("is paused");
+        console.log("is paused");
+        return;
+      }
       // const message = JSON.parse(e.data);
       setReceivedData(e.data);
       console.log(e.data);
     };
-  }, [isPaused]);
+  }, [ws?.current?.onmessage, isPaused]);
+
+  // open change
+  const handleOpenChange = () => {
+    if (!!isOpen) {
+      ws?.current?.close();
+      setIsOpen(false);
+    } else {
+      openSocket();
+      setIsOpen(true);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -45,10 +73,13 @@ const AppWs = () => {
       <button onClick={() => setPause(!isPaused)}>
         {isPaused ? "Resume" : "Pause"}
       </button>
+      <button onClick={() => handleOpenChange()}>
+        {isOpen ? "Close" : "Open"}
+      </button>
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} type="text" />
       </form>
-      <div>{receivedData}</div>
+      <div>Response: {receivedData}</div>
     </div>
   );
 };
