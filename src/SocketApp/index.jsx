a30da71e63ef782ac20708bfdd70a1b0f71d918a
AJTJ <janke.aaron@gmail.com>
2021-05-12 21:18:23 -0400

    now includes sign up process

--- src/SocketApp/index.jsx
+++ src/SocketApp/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 
 /**
+
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
     id: i64,
@@ -17,15 +18,56 @@ struct DatabaseMessage {
     time: NaiveDateTime,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientResponse {
+    message: String,
+    is_sign_in: bool,
+    is_sign_up: bool,
+    user_name: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientMessage {
+    user_id: i64,
+    room_id: i64,
+    message: String,
+    #[serde(default = "default_time")]
+    time: NaiveDateTime,
+}
+
 fn default_time() -> NaiveDateTime {
     Utc::now().naive_utc()
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ResponseToClient {
+    all_messages: String,
+    signed_in: bool,
+    #[serde(default = "default_id")]
+    id: String,
+}
+
+fn default_id() -> String {
+    "Anonymous".to_string()
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Room {
     id: i64,
     name: String,
 }
+
+struct WebSockActor {
+    all_messages: serde_json::Value,
+    db_pool: web::Data<SqlitePool>,
+    id: Identity,
+}
+
  */
+
+const socket_address = "ws://127.0.0.1:8081/ws/";
+const api_address = "http://127.0.0.1:8081";
 
 const AppWs = () => {
   const [isPaused, setPause] = useState(false);
@@ -34,9 +76,13 @@ const AppWs = () => {
   const [messageValue, setMessageValue] = useState(undefined);
   const ws = useRef(null);
 
-  const allMessages = receivedData ? JSON.parse(receivedData) : [];
+  const receivedDataJSON = receivedData ? JSON.parse(receivedData) : {};
 
-  const socket_address = "ws://127.0.0.1:8081/ws/";
+  console.log({ json: receivedDataJSON?.all_messages });
+
+  const allMessages = receivedDataJSON?.all_messages
+    ? JSON.parse(receivedDataJSON.all_messages)
+    : [];
 
   const openSocket = () => {
     ws.current = new WebSocket(socket_address);
@@ -88,7 +134,16 @@ const AppWs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    ws.current.send(messageValue);
+
+    const jsonObject = JSON.stringify({
+      message: messageValue,
+      is_sign_in: false,
+      is_sign_up: true,
+      user_name: "Henry",
+      password: "password",
+    });
+
+    ws.current.send(jsonObject);
   };
 
   // Eventually user_id should be set somewhere and same with room_id
@@ -128,10 +183,55 @@ const AppWs = () => {
   );
 };
 
+const SignUp = () => {
+  const [userName, setUserName] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user_name: userName, password: password }),
+    };
+    fetch(`${api_address}/signup/`, requestOptions).then((response) =>
+      console.log("the signup response:", response)
+    );
+  };
+
+  const handleUserChange = (e) => {
+    setUserName(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>Login</div>
+      <form onSubmit={handleSignUp}>
+        <label htmlFor="userInput">
+          Name
+          <input id="userInput" type="text" onChange={handleUserChange} />
+        </label>
+        <label htmlFor="passwordInput">
+          password
+          <input
+            id="passwordInput"
+            type="password"
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
       <div>WS APP</div>
+      <SignUp />
       <AppWs />
     </div>
   );
